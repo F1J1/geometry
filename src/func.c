8ee7c37170cf48e4d6249a9f2eceafaedc6a6c78
root <root@localhost.localdomain>
2019-05-13 23:56:46 +0700

    Add the last update to func.c

--- src/func.c
+++ src/func.c
@@ -1,13 +1,12 @@
 #include "func.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 float peresechenie = 0, r = 0;
 
-void Math (float x1, float y1, float r1, float x2, float y2, float r2)
-{
-	r = r1 + r2;
-	peresechenie = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+void Math(float x1, float y1, float r1, float x2, float y2, float r2) {
+  r = r1 + r2;
+  peresechenie = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
